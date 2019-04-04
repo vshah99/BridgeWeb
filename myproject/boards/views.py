30e241b5f531a917f8442595736ee4064a43d044
vedantshah <vedant.shah@mail.utoronto.ca>
2019-04-03 21:27:59 -0400

    first

---
+++ myproject/boards/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Board
+
+def home(request):
+    boards = Board.objects.all()
+    return render(request, 'index.html', {'boards': boards})
+
